2a259049edd90fdbbce96383acfb3e7078b34e50
Avinash-Sahani <avinashsahani66@gmail.com>
2025-04-25 12:12:59 -0400

    create validation helper methods

--- Period-Based-Encounter/Models/AdtEventRecord.cs
+++ Period-Based-Encounter/Models/AdtEventRecord.cs
@@ -5,7 +5,7 @@ public class AdtEventRecord
     public int EventId { get; set; } 
     public int PatientId { get; set; }
     public string HospitalName { get; set; }
-    public DateTime? EventDate { get; set; }
+    public DateOnly? EventDate { get; set; }
     public EventType EventType { get; set; } 
     public string? LevelOfCare { get; set; }
     public string? ChiefComplaint { get; set; }

6849a2fb6b99b8d3b33c8ff5e3402cff2b10a542
Avinash-Sahani <avinashsahani66@gmail.com>
2025-04-26 00:07:35 -0400

    create encoruter processor and write the core logic in that class

--- Period-Based-Encounter/Processor/EncounterProcessor.cs
+++ Period-Based-Encounter/Processor/EncounterProcessor.cs
@@ -1,3 +1,4 @@
+using Period_Based_Encounter.Constants;
 using Period_Based_Encounter.Helpers;
 using Period_Based_Encounter.Models;
 
@@ -5,45 +6,98 @@ namespace Period_Based_Encounter;
 
 public class EncounterProcessor
 {
+    private readonly Dictionary<int, AdtEventRecord> _eventRecords = new();
+    private readonly List<EncounterRecord> _validRecords = new();
+    private readonly List<InvalidEncounterRecord> _invalidRecords = new();
 
-    public Dictionary<int, AdtEventRecord> _eventRecords;
-   
-    public EncounterProcessor()
+    public (List<EncounterRecord>, List<InvalidEncounterRecord>) GetRecords()
     {
-
-        _eventRecords = new Dictionary<int, AdtEventRecord>();
+        return (_validRecords, _invalidRecords);
     }
-    
-    public void ProcessRecord(AdtEventRecord record)
+    public void ProcessRecords(List<AdtEventRecord> records)
     {
+        foreach (var record in records)
+            ProcessRecord(record);
 
-        if (!ContainsRecord(record.PatientId))
+        AddRemainingAdmitOnlyRecords();
+    }
+
+    private void ProcessRecord(AdtEventRecord record)
+    {
+        if (!_eventRecords.TryGetValue(record.PatientId, out var currentRecord))
         {
-            if(ValidationHelper.IsAdmit(record.EventType))
-                    AddEventRecord(record);
-            
-            if (ValidationHelper.IsDischarge(record.EventType))
-            {
-                // Its a Invalid Record
-            }
-            
+            HandleNewRecord(record);
         }
         else
         {
-            var currentRecord = _eventRecords[record.PatientId];
-            if (ValidationHelper.IsDischarge(record.EventType))
-            {
-                if (ValidationHelper.IsHospitalNameSame(record.HospitalName, currentRecord.HospitalName))
-                {
-                    if (ValidationHelper.IsStartDateBeforeOrEqualEndDate(currentRecord.EventDate, record.EventDate))
-                    {
-                        // Process the record
-                    }
-                }
-            }
+            HandleExistingRecord(record, currentRecord);
         }
     }
 
-    private bool ContainsRecord(int PatinetId) => _eventRecords.ContainsKey(PatinetId);
-    private void AddEventRecord(AdtEventRecord record) => _eventRecords.Add(record.PatientId, record);
+    private void HandleNewRecord(AdtEventRecord record)
+    {
+        if (ValidationHelper.IsAdmit(record.EventType))
+        {
+            AddEventRecord(record);
+        }
+        else if (ValidationHelper.IsDischarge(record.EventType))
+        {
+            _invalidRecords.Add(record.ToInvalid(string.Format(ErrorMessages.DischargeBeforeAdmit, record.PatientId)));
+        }
+    }
+
+    private void HandleExistingRecord(AdtEventRecord newRecord, AdtEventRecord currentRecord)
+    {
+        if (ValidationHelper.IsDischarge(newRecord.EventType))
+        {
+            ProcessDischarge(newRecord, currentRecord);
+        }
+        else if (ValidationHelper.IsAdmit(newRecord.EventType))
+        {
+            ProcessAdmit(newRecord, currentRecord);
+        }
+    }
+
+    private void ProcessDischarge(AdtEventRecord discharge, AdtEventRecord admit)
+    {
+        if (!ValidationHelper.IsHospitalNameSame(discharge.HospitalName, admit.HospitalName))
+        {
+            _invalidRecords.Add(discharge.ToInvalid(string.Format(ErrorMessages.DischargeDifferentHospital, discharge.HospitalName, admit.HospitalName)));
+            return;
+        }
+
+        if (!ValidationHelper.IsStartDateBeforeOrEqualEndDate(admit.EventDate, discharge.EventDate))
+        {
+            _invalidRecords.Add(discharge.ToInvalid(string.Format(ErrorMessages.DischargeBeforeAdmitDate, admit.EventDate, discharge.EventDate)));
+            return;
+        }
+
+        _validRecords.Add(AdtEventRecord.ToEncounter(admit, discharge));
+        _eventRecords.Remove(discharge.PatientId);
+    }
+
+    private void ProcessAdmit(AdtEventRecord newAdmit, AdtEventRecord oldAdmit)
+    {
+        if (!ValidationHelper.IsHospitalNameSame(newAdmit.HospitalName, oldAdmit.HospitalName))
+        {
+            _invalidRecords.Add(newAdmit.ToInvalid(string.Format(ErrorMessages.AdmitToNewHospitalWithoutDischarge, oldAdmit.HospitalName, newAdmit.HospitalName)));
+            return;
+        }
+        oldAdmit.LevelOfCare = newAdmit.LevelOfCare;
+        oldAdmit.ChiefComplaint = newAdmit.ChiefComplaint;
+        oldAdmit.EventId = newAdmit.EventId;
+    }
+
+    private void AddEventRecord(AdtEventRecord record)
+    {
+        _eventRecords[record.PatientId] = record;
+    }
+
+    private void AddRemainingAdmitOnlyRecords()
+    {
+        foreach (var record in _eventRecords.Values)
+        {
+            _validRecords.Add(record.ToEncounter());
+        }
+    }
 }
